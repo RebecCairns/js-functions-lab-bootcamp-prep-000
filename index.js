ba851f8099a6c46a32499385c4a95761cc65fb4b
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-11 20:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidaysTo(holiday, name){
   return "Happy ${holiday}, ${name}";
 }
-function holidayCountdown(holiday, numDays){
-  return "It's ${numDays} days until ${holiday}!"
+function holidayCountdown(holiday, days){
+  return "It's ${days} days until ${holiday}!"
 }
